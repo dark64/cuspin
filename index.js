3359827d48d7d739568953f8f18426a570c59164
dark64 <darem966@gmail.com>
2019-10-16 01:09:01 +0200

    fix unsubscribing

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ var __subscribes = {}
  */
 function subscribe(name, listener) {
     if (!__subscribes[name]) {
-        __subscribes[name] = [];
+        __subscribes[name] = { last: 0, listeners: {} };
     }
-    const index = __subscribes[name].push(listener) - 1;
+
+    const index = __subscribes[name].last;
+    __subscribes[name].listeners[index] = listener;
+    __subscribes[name].last = (++__subscribes[name].last) % Number.MAX_SAFE_INTEGER;
+
     return () => {
-        __subscribes[name].splice(index, 1);
+        delete __subscribes[name].listeners[index]; 
     }
 }
 
@@ -23,8 +27,13 @@ function subscribe(name, listener) {
  * @param {any} args Event arguments
  */
 function emit(name, args) {
-    if (!__subscribes[name]) return;
-    __subscribes[name].forEach(listener => listener(args))
+    if (!__subscribes[name]) {
+        return;
+    }
+    const listeners = __subscribes[name].listeners;
+    for (var index in listeners) {
+        listeners[index](args);
+    }
 }
 
 export const subscribe = subscribe;
